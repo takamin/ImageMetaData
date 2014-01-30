a7c0f62bf9347bafa3227ae3d473751f0ce362c9
Koji Takami <vzg03566@gmail.com>
2014-01-30 20:12:22 +0900

    change form design.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -52,7 +52,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(388, 19);
+            this.panel1.Size = new System.Drawing.Size(682, 19);
             this.panel1.TabIndex = 1;
             // 
             // panel7
@@ -61,7 +61,7 @@
             this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel7.Location = new System.Drawing.Point(61, 0);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(327, 19);
+            this.panel7.Size = new System.Drawing.Size(621, 19);
             this.panel7.TabIndex = 1;
             // 
             // labelFilename
@@ -70,7 +70,7 @@
             this.labelFilename.Dock = System.Windows.Forms.DockStyle.Fill;
             this.labelFilename.Location = new System.Drawing.Point(0, 0);
             this.labelFilename.Name = "labelFilename";
-            this.labelFilename.Size = new System.Drawing.Size(327, 19);
+            this.labelFilename.Size = new System.Drawing.Size(621, 19);
             this.labelFilename.TabIndex = 0;
             this.labelFilename.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -99,7 +99,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 19);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(141, 243);
+            this.panel2.Size = new System.Drawing.Size(313, 249);
             this.panel2.TabIndex = 2;
             // 
             // pictureBox
@@ -107,7 +107,7 @@
             this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox.Location = new System.Drawing.Point(0, 0);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(141, 243);
+            this.pictureBox.Size = new System.Drawing.Size(313, 249);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
@@ -117,9 +117,9 @@
             this.panel3.Controls.Add(this.panel5);
             this.panel3.Controls.Add(this.panel4);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(141, 19);
+            this.panel3.Location = new System.Drawing.Point(313, 19);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(247, 243);
+            this.panel3.Size = new System.Drawing.Size(369, 249);
             this.panel3.TabIndex = 3;
             // 
             // panel5
@@ -128,7 +128,7 @@
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel5.Location = new System.Drawing.Point(0, 21);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(247, 222);
+            this.panel5.Size = new System.Drawing.Size(369, 228);
             this.panel5.TabIndex = 1;
             // 
             // textBoxMetaData
@@ -138,7 +138,7 @@
             this.textBoxMetaData.Multiline = true;
             this.textBoxMetaData.Name = "textBoxMetaData";
             this.textBoxMetaData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxMetaData.Size = new System.Drawing.Size(247, 222);
+            this.textBoxMetaData.Size = new System.Drawing.Size(369, 228);
             this.textBoxMetaData.TabIndex = 0;
             // 
             // panel4
@@ -147,7 +147,7 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(247, 21);
+            this.panel4.Size = new System.Drawing.Size(369, 21);
             this.panel4.TabIndex = 0;
             // 
             // label3
@@ -155,7 +155,7 @@
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Location = new System.Drawing.Point(0, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(247, 21);
+            this.label3.Size = new System.Drawing.Size(369, 21);
             this.label3.TabIndex = 0;
             this.label3.Text = "meta data";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -165,7 +165,7 @@
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 262);
+            this.ClientSize = new System.Drawing.Size(682, 268);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
